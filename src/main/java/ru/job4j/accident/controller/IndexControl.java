881e5273dd4234881176bae83787c101940e93f4
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2022-08-30 21:58:16 +1000

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accident/controller/IndexControl.java
+++ src/main/java/ru/job4j/accident/controller/IndexControl.java
@@ -1,12 +1,17 @@
 package ru.job4j.accident.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexControl {
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("users", List.of("Anna", "Dmitriy", "Irina"));
         return "index";
     }
 }
